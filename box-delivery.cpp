44682c19dac2c470a7de13f7da653fa3f53f81c5
ragnarlodbrok1992 <maraus92@gmail.com>
2023-08-22 19:52:06 +0200

    First commit

---
+++ box-delivery.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+// #include <glad\glad.h>
+
+int main(int argc, char* argv[])
+{
+  // First message of engine
+  printf("box-delivery engine - development version.\n");
+
+  // Last message of engine
+  printf("All went well. Exiting.\n");
+  return 0;
+}
---
+++ build.bat
@@ -0,0 +1,23 @@
+@echo off
+SET compiler_dir="C:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Tools\MSVC\14.31.31103\bin\Hostx64\x64\cl.exe"
+
+SET header_file_glfw3="D:\libraries\glfw-3.3.8\include"
+SET header_file_glad="D:\libraries\glad\include"
+SET lib_file_glfw="D:\libraries\glfw-3.3.8\build\src\Debug\glfw3.lib"
+SET glad_main_file="D:\libraries\glad\src\glad.c"
+SET glm_header_files="D:\libraries\glm"
+
+REM /Wall - disabling for now @TODO: make warning sensible
+REM /link /LIBPATH:%lib_file_glfw%^
+REM /link /verbose:lib^
+
+%compiler_dir% /EHsc /Zi /MDd^
+  /DEBUG:FULL^
+  /INCREMENT:NO^
+  /std:c++17^
+  /Fe:"box-delivery"^
+  %glad_main_file%^
+  box-delivery.cpp^
+  /I%header_file_glfw3% /I%header_file_glad% /I%glm_header_files%^
+  OpenGL32.lib User32.lib gdi32.lib Shell32.lib %lib_file_glfw%^
+
